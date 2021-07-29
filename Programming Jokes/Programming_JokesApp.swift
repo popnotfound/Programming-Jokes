3540ee4707e17f586941b9a2cf4562ba308cd6e4
popnotfound <aneeshleisure21@gmail.com>
2021-07-28 21:21:03 -0500

    Initial Commit

---
+++ Programming Jokes/Programming_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_JokesApp.swift
+//  Programming Jokes
+//
+//  Created by Aneesh Pushparaj on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Programming_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
