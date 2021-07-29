ffaa658f1330765372099a222429d72183460990
popnotfound <aneeshleisure21@gmail.com>
2021-07-28 21:43:01 -0500

    Finished setting up API alert & Finished Product

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var jokes = [joke]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
             List(jokes) { joke in
@@ -19,15 +20,37 @@ struct ContentView: View {
                         Text(joke.setup)
                     })
             }
-            .navigationTitle("Programming Jokes")
+            .navigationBarTitle("Programming Jokes", displayMode: .inline)
         }
         .onAppear(perform: {
             getJokes()
         })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
+        })
     }
     
     func getJokes() {
-        let apiKey = "?rapidapi-key = 4889f9e3f9msh837983776489b8cp12d1ddjsn0fb79206ee5b"
+        let apiKey = "?rapidapi-key=4889f9e3f9msh837983776489b8cp12d1ddjsn0fb79206ee5b"
+        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["success"] == true {
+                    let contents = json["body"].arrayValue
+                    for item in contents {
+                        let setup = item["setup"].stringValue
+                        let punchline = item["punchline"].stringValue
+                        let joke = joke(setup: setup, punchline: punchline)
+                        jokes.append(joke)
+                    }
+                    return
+                }
+            }
+        }
+        showingAlert = true
     }
 }
 
